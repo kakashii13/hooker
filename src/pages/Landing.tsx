f3d11616a863ae4ce62822042295dab2f77ae219
Matias Dominguez <petunilo96@gmail.com>
2022-08-03 09:28:26 -0300

    update

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -4,7 +4,7 @@ import { Login } from "../Login";
 
 export const Landing = () => {
   return (
-    <VStack justifyContent="center" h="100vh" spacing={10}>
+    <VStack justifyContent="center" h="100vh" spacing={10} textAlign="center">
       <Image src={logo} w="100px" />
       <Heading fontSize="50px">Share with Hooker</Heading>
       <Heading as="h2">Join now.</Heading>
